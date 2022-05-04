8bb4616bb003bb211ab3bcd30478fbf9bc9a578e
jaboialexT <Fartypoopybutt@gmail.com>
2022-05-04 01:36:32 -0400

    Change the Timer format to remove leading 0s

--- HoldThisButtonDown/Assets/TimerController.cs
+++ HoldThisButtonDown/Assets/TimerController.cs
@@ -22,7 +22,7 @@ public class TimerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeCounter.text = "0.00:00:00.00";
+        timeCounter.text = "0";
         timerGoing = false;
     }
 
@@ -43,11 +43,38 @@ public class TimerController : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             timePlaying = TimeSpan.FromSeconds(elapsedTime);
-            string timePlayingStr = timePlaying.ToString("d'.'hh':'mm':'ss'.'ff");
+            string timePlayingStr = GetSimplestTimeSpan(timePlaying);
             timeCounter.text = timePlayingStr;
 
             yield return null;
         }
+    }
+
+    public static string GetSimplestTimeSpan(TimeSpan timeSpan)
+    {
+        var result = string.Empty;
+        if (timeSpan.Days > 0)
+        {
+            result += string.Format(@"{0:ddd\.}", timeSpan).TrimStart('0');
+        }
+        if (timeSpan.Hours > 0)
+        {
+            result += string.Format( @"{0:hh\:}", timeSpan).TrimStart('0');
+        }
+        if (timeSpan.Minutes > 0)
+        {
+            result += string.Format(@"{0:mm\:}", timeSpan).TrimStart('0');
+        }
+        if (timeSpan.Seconds > 0)
+        {
+            result += string.Format(@"{0:ss\:}", timeSpan).TrimStart('0');
+        }
+        if (timeSpan.Milliseconds > 0)
+        {
+            result += string.Format( @"{0:ff}", timeSpan).TrimStart('0');
+            
+        }
+        return result;
     }
 
 }
